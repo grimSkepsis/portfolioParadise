e08adec6d7adda85791394088668800417cd41f0
Michael Hofer <mhofer180993@gmail.com>
2017-11-02 07:44:56 -0700

    Adds skeleton components

--- portolio-app/src/app/app.module.ts
+++ portolio-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { StyleguideComponent } from './styleguide/styleguide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    AboutmeComponent,
+    StyleguideComponent
   ],
   imports: [
     BrowserModule
