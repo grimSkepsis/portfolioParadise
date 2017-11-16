a95e7bb680d3807867cc33a85a9b2024a87f6dca
Michael Hofer <mhofer180993@gmail.com>
2017-11-15 20:49:09 -0800

    tweaks site layout some

--- portolio-app/src/app/home/home.component.ts
+++ portolio-app/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +10,7 @@ export class HomeComponent implements OnInit {
   constructor(
 
   ) { }
-
-  public opacityValue: number = 0;
   ngOnInit() {
-  }
-
-  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
-    this.opacityValue = window.pageYOffset/(600);
-    if(this.opacityValue > 0.4) this.opacityValue = 0.4;
-    if(this.opacityValue < 0) this.opacityValue = 0;
   }
 
 }
